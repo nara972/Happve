1571723b660592933835370c6bb8b2aecc0c1d9a
nara972 <68370958+nara972@users.noreply.github.com>
2021-08-09 20:18:06 +0900

    햅비 프로젝트 시작

---
+++ Happve/src/main/java/com/kh/happve/HappveApplication.java
@@ -0,0 +1,13 @@
+package com.kh.happve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappveApplication.class, args);
+	}
+
+}
---
+++ Happve/src/main/java/com/kh/happve/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.kh.happve.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity 
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.authorizeRequests()
+		    .mvcMatchers("/","/login","/signup").permitAll()
+		    .anyRequest().authenticated();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception{
+		web.ignoring()
+		   .antMatchers("/favicon.ico", "/resources/**", "/error")
+		   .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+	}
+
+}
