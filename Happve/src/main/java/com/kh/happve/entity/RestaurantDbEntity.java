526b72d46af3fd04abc9d4f4875041bbc596c3cd
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2021-08-27 15:27:15 +0900

    RestaurantDbEntity update
    수정

--- Happve/src/main/java/com/kh/happve/entity/RestaurantDbEntity.java
+++ Happve/src/main/java/com/kh/happve/entity/RestaurantDbEntity.java
@@ -1,18 +1,17 @@
+package com.kh.happve.entity;
 
-        package com.kh.happve.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-        import lombok.AllArgsConstructor;
-        import lombok.Builder;
-        import lombok.Data;
-        import lombok.NoArgsConstructor;
-
-        import javax.persistence.*;
-        import javax.validation.constraints.NotBlank;
-        import java.math.BigInteger;
-        import java.time.LocalDateTime;
-        import java.util.ArrayList;
-        import java.util.List;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -23,30 +22,32 @@
 @Table(name="RestaurantDbEntity")
 public class RestaurantDbEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="restaurant_id")
-    private Long restaurantId; //식당 고유 id
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name="restaurant_id")
+        private Long restaurantId; //식당 고유 id
 
-    @NotBlank
-    private String title;  //식당 이름
 
-    @Column(nullable=true)
-    private String category; // 업태명 (ex 한식, 경양식, 카페 등)
+        
+        @NotBlank
+        private String title;  //식당 이름
 
-    @Column(nullable=true)
-    private String address; // 도로명 상세주소
+        @Column(nullable=true)
+        private String category; // 업태명 (ex 한식, 경양식, 카페 등)
 
-    @NotBlank
-    private String roadAddress; //도로명주소
+        @Column(nullable=true)
+        private String address; // 도로명 상세주소
 
-    private String tel;  //업체 전화번호
+        @NotBlank
+        private String roadAddress; //도로명주소
 
-    @Column(nullable=true)
-    private String imageLink; //이미지 링크
+        private String tel;  //업체 전화번호
 
-    @Column(nullable=false)
-    private String menu;
+        @Column(nullable=true)
+        private String imageLink; //이미지 링크
+
+        @Column(nullable=false)
+        private String menu;
 
 
 
