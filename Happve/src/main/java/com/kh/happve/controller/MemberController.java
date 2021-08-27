a997065972e09ebfe65abad75f8eb9c4f8e791a9
nara972 <68370958+nara972@users.noreply.github.com>
2021-08-27 19:03:52 +0900

    마이페이지 수정 기능/시큐리티 설정 추가 Repush

--- Happve/src/main/java/com/kh/happve/controller/MemberController.java
+++ Happve/src/main/java/com/kh/happve/controller/MemberController.java
@@ -8,10 +8,14 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.kh.happve.dto.PasswordForm;
+import com.kh.happve.dto.ProfileForm;
 import com.kh.happve.dto.SignUpForm;
 import com.kh.happve.entity.Member;
 import com.kh.happve.service.MemberService;
 import com.kh.happve.validator.CurrentAccount;
+import com.kh.happve.validator.PasswordFormValidator;
+import com.kh.happve.validator.ProfileFormValidator;
 import com.kh.happve.validator.SignUpFormValidator;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +25,8 @@ import lombok.RequiredArgsConstructor;
 public class MemberController {
 	
 	private final SignUpFormValidator signUpFormValidator;
+	private final ProfileFormValidator profileValidator;
+	private final PasswordFormValidator passwordValidator;
 	private final MemberService memberService;
 	
 	@GetMapping("/admin")
@@ -49,5 +55,50 @@ public class MemberController {
 		
 		return "redirect:/";
 	}
+	
+	//마이페이지-프로필 변경
+			@GetMapping("/mypage/updateprofile")
+			public String updateProfileForm(@CurrentAccount Member member,Model model) {
+				model.addAttribute(member);
+				model.addAttribute("profileForm",new ProfileForm());
+				return "updateprofile";
+			}
+			
+			@PostMapping("/mypage/updateprofile")
+			public String updateProfile(@CurrentAccount Member member,@Valid ProfileForm profileForm,Errors errors,
+					Model model) {
+				if(errors.hasErrors()) {
+					return "updateprofile";
+				}
+				profileValidator.validate(profileForm, errors);
+				if(errors.hasErrors()) {
+					return "updateprofile";
+				}
+				memberService.updateProfile(member, profileForm.getEmail(), profileForm.getNickname(), profileForm.getVtype());
+				model.addAttribute(member);
+				return "redirect:/mypage";
+			}
+			
+			//마이페이지-비밀번호 변경
+			@GetMapping("/mypage/updatepassword")
+			public String updatePasswordForm(@CurrentAccount Member member,Model model) {
+				model.addAttribute("passwordForm",new PasswordForm());
+				return "updatepassword";
+			}
+			
+			@PostMapping("/mypage/updatepassword")
+			public String updatePassword(@CurrentAccount Member member, @Valid PasswordForm passwordForm, Errors errors,
+					Model model) {
+				if(errors.hasErrors()) {
+					return "updatepassword";
+				}
+				passwordValidator.validate(passwordForm, errors);
+				if(errors.hasErrors()) {
+					return "updatepassword";
+				}
+				
+				memberService.updatePassword(member, passwordForm.getNewPassword());
+				return "redirect:/mypage";
+			}
 
 }
