726416e847e005fe14992967d26b31d6f173f87b
ShaunAUS <86937655+ShaunAUS@users.noreply.github.com>
2021-08-29 12:59:54 +0900

    검색 쿼리 수정
    검색 쿼리 수정

--- Happve/src/main/java/com/kh/happve/repository/RestaurantRepository.java
+++ Happve/src/main/java/com/kh/happve/repository/RestaurantRepository.java
@@ -16,18 +16,18 @@ public interface RestaurantRepository extends JpaRepository<RestaurantDbEntity,L
 
 
     //식당 이름으로 검색
-    @Query(value="SELECT * from restaurant_db_entity where restaurant_db_entity.title like ?1%",nativeQuery = true)
+    @Query(value="SELECT * from restaurant_db_entity where restaurant_db_entity.title like %?1%",nativeQuery = true)
     List<RestaurantDbEntity> getListByTitle(String keyword);
 
 
     //메뉴로 검색
-    @Query(value="SELECT * from restaurant_db_entity where restaurant_db_entity.menu like ?1%",nativeQuery = true)
+    @Query(value="SELECT * from restaurant_db_entity where restaurant_db_entity.menu like %?1%",nativeQuery = true)
     List<RestaurantDbEntity> getListByMenu(String keyword);
 
     //상단검색 ( 식당+이름 검색)
     @Query(value="SElECT * from restaurant_db_entity " +
-            "where restaurant_db_entity.menu like :keyword% or" +
-            " restaurant_db_entity.title like :keyword% ",nativeQuery = true)
+            "where restaurant_db_entity.menu like %:keyword% or" +
+            " restaurant_db_entity.title like %:keyword% ",nativeQuery = true)
     List<RestaurantDbEntity> getListByAllSearch(@Param("keyword") String keyword);
 
 }
