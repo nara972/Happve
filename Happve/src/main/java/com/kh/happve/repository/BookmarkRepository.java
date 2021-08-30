f819bcb9700bc626da9552eb31ca3698095f0aa3
nara972 <68370958+nara972@users.noreply.github.com>
2021-08-30 15:07:37 +0900

    즐겨찾기 기능 구현 완료

---
+++ Happve/src/main/java/com/kh/happve/repository/BookmarkRepository.java
@@ -0,0 +1,22 @@
+package com.kh.happve.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.kh.happve.entity.Bookmark;
+import com.kh.happve.entity.Member;
+
+@Transactional(readOnly = true)
+public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+	
+	Bookmark findByMemberIdAndRestaurantId(Long memberId, Long restaurantId);
+
+	void deleteByMemberIdAndRestaurantId(Long memberId, Long restaurantId);
+
+	Bookmark findByMemberId(Long memberId);
+
+	List<Bookmark> findByRestaurantId(Long restaurantId);
+
+}
---
+++ Happve/src/main/java/com/kh/happve/service/BookmarkService.java
@@ -0,0 +1,47 @@
+package com.kh.happve.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.kh.happve.entity.Bookmark;
+import com.kh.happve.entity.Member;
+import com.kh.happve.entity.RestaurantDbEntity;
+import com.kh.happve.repository.BookmarkRepository;
+import com.kh.happve.repository.RestaurantRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BookmarkService {
+
+	private final BookmarkRepository bookmarkRepository;
+	private final RestaurantRepository restaurantRepository;
+
+	// 북마크 추가
+	public Bookmark create(Bookmark bookmark) {
+		return bookmarkRepository.save(bookmark);
+	}
+
+	// 북마크 삭제
+	public void delete(Long memberId, Long restaurantId) {
+		bookmarkRepository.deleteByMemberIdAndRestaurantId(memberId, restaurantId);
+	}
+	
+	public Bookmark findByMemAndResId(Long memberId, Long restaurantId) {
+		return bookmarkRepository.findByMemberIdAndRestaurantId(memberId, restaurantId);
+	}
+
+	public Bookmark BookmarkResId(Long memberId) {
+		return bookmarkRepository.findByMemberId(memberId);
+	}
+
+	public List<RestaurantDbEntity> BookmarkList() {
+		return restaurantRepository.findAll();
+	}
+
+}
